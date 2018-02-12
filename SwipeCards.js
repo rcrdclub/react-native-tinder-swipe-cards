84e290eaf849bed931e58fac19246fdbe55b09ac
Damian Manning <damian@manning.net>
2018-02-12 02:28:40 -0500

    Force swipe up bug fix

--- SwipeCards.js
+++ SwipeCards.js
@@ -269,7 +269,7 @@ export default class SwipeCards extends Component {
 
   _forceUpSwipe() {
     this.cardAnimation = Animated.timing(this.state.pan, {
-      toValue: { x: 0, y: 500 },
+      toValue: { x: 0, y: -500 },
     }).start(status => {
       if (status.finished) {
         this.props.handleMaybe(this.state.card);
